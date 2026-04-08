88a0769b21f8d44dc37022059f362009d05b48f8
shamamasiddiqui95-tech <shamamasiddiqui95@gmail.com>
2026-04-08 13:39:49 +0530

    Update Main.java

--- src/q03/Main.java
+++ src/q03/Main.java
@@ -1,58 +1,46 @@
-package.q03
-    import java.util.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-
-        int[][] arr = new int[n][n];
-
+        
+        int[][] a = new int[n][n];
+        
         int top = 0, bottom = n - 1;
         int left = 0, right = n - 1;
         int num = 1;
- // Fill matrix in spiral order
-        while (top <= bottom && left <= right) {
 
-            // Left → Right
-            for (int i = left; i <= right; i++) {
-                arr[top][i] = num++;
-            }
+        while (true) {
+            for (int i = left; i <= right; i++) a[top][i] = num++;
             top++;
- // Top → Bottom
-            for (int i = top; i <= bottom; i++) {
-                arr[i][right] = num++;
-            }
+            if (top > bottom) break;
+
+            for (int i = top; i <= bottom; i++) a[i][right] = num++;
             right--;
-// Right → Left
-            if (top <= bottom) {
-                for (int i = right; i >= left; i--) {
-                    arr[bottom][i] = num++;
-                }
-                bottom--;
-            }
- // Bottom → Top
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    arr[i][left] = num++;
-                }
-                left++;
-            }
+            if (left > right) break;
+
+            for (int i = right; i >= left; i--) a[bottom][i] = num++;
+            bottom--;
+            if (top > bottom) break;
+
+            for (int i = bottom; i >= top; i--) a[i][left] = num++;
+            left++;
+            if (left > right) break;
         }
-        // Print matrix
+
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                System.out.print(arr[i][j]);
+                System.out.print(a[i][j]);
                 if (j < n - 1) System.out.print(" ");
             }
             System.out.println();
         }
- // Calculate primary diagonal sum
-        int diagonalSum = 0;
-        for (int i = 0; i < n; i++) {
-            diagonalSum += arr[i][i];
-        }
 
-        System.out.println("Diagonal: " + diagonalSum);
+        int sum = 0;
+        for (int i = 0; i < n; i++)
+            sum += a[i][i];
+
+        System.out.println("Diagonal: " + sum);
     }
-                    }
+}
